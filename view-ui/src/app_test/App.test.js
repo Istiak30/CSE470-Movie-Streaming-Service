1d4df2497c2f28a8645834320e22611a0d39e18f
Istiak <istiakahmedarnob99@gmail.com>
2023-05-04 06:32:26 +0600

    Initial

--- view-ui/src/app_test/App.test.js
+++ view-ui/src/app_test/App.test.js
@@ -1,8 +1,45 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import { MemoryRouter } from 'react-router-dom';
+import App from '../App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App component', () => {
+  test('renders the Login page on the /login route', () => {
+    render(
+      <MemoryRouter initialEntries={['/login']}>
+        <App />
+      </MemoryRouter>
+    );
+
+    expect(screen.getByText('Login')).toBeInTheDocument();
+  });
+
+  test('renders the Netflix page on the / route', () => {
+    render(
+      <MemoryRouter initialEntries={['/']}>
+        <App />
+      </MemoryRouter>
+    );
+
+    expect(screen.getByText('Home Page')).toBeInTheDocument();
+  });
+
+  test('renders the Login page on the /signup route', () => {
+    render(
+      <MemoryRouter initialEntries={['/signup']}>
+        <App />
+      </MemoryRouter>
+    );
+
+    expect(screen.getByText('Signup')).toBeInTheDocument();
+  });
+
+  test('renders the Netflix page on the /tv route', () => {
+    render(
+      <MemoryRouter initialEntries={['/tv']}>
+        <App />
+      </MemoryRouter>
+    );
+
+    expect(screen.getByText('TV Series')).toBeInTheDocument();
+  });  
 });
